9cbe48f6b6bb05e841e88af805a35e60470016cb
Adam Horo <loginadas@gmail.com>
2025-12-25 16:07:21 +0100

    cards assets WIP

--- Assets/Scripts/UI/UIButton.cs
+++ Assets/Scripts/UI/UIButton.cs
@@ -20,6 +20,11 @@ public class UIButton : MonoBehaviour, ISelectHandler, IPointerEnterHandler
 
     public void OnSelect(BaseEventData eventData)
     {
+        if (panel == null)
+        {
+            Debug.LogWarning("UIButton OnSelect called before initialization.");
+            return;
+        }
         panel.SelectedButtonId = buttonId;
         panel.SelectedButton = button;
     }
