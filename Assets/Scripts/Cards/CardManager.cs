b8edc14f22ea7c24bef12cd9880307a254d371d1
Adam Horo <loginadas@gmail.com>
2026-01-02 16:57:29 +0100

    asset import

--- Assets/Scripts/Cards/CardManager.cs
+++ Assets/Scripts/Cards/CardManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CardManager : MonoBehaviour
@@ -11,9 +12,16 @@ public class CardManager : MonoBehaviour
     [SerializeField] private Transform cardDragParent;
     [SerializeField] private Canvas canvas;
     [SerializeField] private int cardNumber = 5;
+    [SerializeField] private int maxCards = 6;
+    [SerializeField] private int minNumberCards = 2;
+    [SerializeField] private int minSymbolCards = 2;
 
     private List<Card> currentHand = new List<Card>();
     private string currentExpression = "";
+    private int numberOfNumberCards;
+    private int numberOfSymbolCards;
+    private List<CardDataSO> numberCards;
+    private List<CardDataSO> symbolCards;
 
     private void Awake()
     {
@@ -22,19 +30,42 @@ public class CardManager : MonoBehaviour
 
     private void Start()
     {
+        numberCards = cardDataList.Where(c => c.type == CardType.Number).ToList();
+        symbolCards = cardDataList.Where(c => c.type != CardType.Number).ToList();
         DrawCards(); // Dobieramy karty na start
     }
 
     public void DrawCards(int numberOfCards)
     {
-        for (int i = 0; i < numberOfCards; i++)
+        if (numberOfCards > maxCards)
         {
-            CardDataSO randomCard = cardDataList[UnityEngine.Random.Range(0, cardDataList.Count)];
-            GameObject newCardObj = Instantiate(cardPrefab, cardParent);
-            Card newCard = newCardObj.GetComponent<Card>();
-            newCard.Initialize(randomCard, this, cardDragParent, canvas);
-            currentHand.Add(newCard);
+            numberOfCards = maxCards;
         }
+
+        for (int i = 0; i < minNumberCards; i++)
+        {
+            DrawRandomFrom(numberCards);
+        }
+
+        for (int i = 0; i < minSymbolCards; i++)
+        {
+            DrawRandomFrom(symbolCards);
+        }
+
+        int remaining = numberOfCards - minNumberCards - minSymbolCards;
+        for (int i = 0; i < remaining; i++)
+        {
+            DrawRandomFrom(cardDataList);
+        }
+    }
+
+    private void DrawRandomFrom(List<CardDataSO> source)
+    {
+        CardDataSO randomCard = source[UnityEngine.Random.Range(0, source.Count)];
+        GameObject newCardObj = Instantiate(cardPrefab, cardParent);
+        Card newCard = newCardObj.GetComponent<Card>();
+        newCard.Initialize(randomCard, this, cardDragParent, canvas);
+        currentHand.Add(newCard);
     }
 
     public void DrawCards()
